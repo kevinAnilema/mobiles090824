d82f155877a5a9bd421a8838f026815c4756369e
Kevin Anilema <kevinanilema2000@gmail.com>
2024-08-09 22:33:27 -0500

    taller 09 08 2024

--- App.tsx
+++ App.tsx
@@ -1,10 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import {initializeApp} from "firebase/app";
+import {getDatabase, ref, onValue, set} from "firebase/database";
+import { useState } from 'react';
 export default function App() {
+  const [score, setScore]=useState<string>('')
+  const [userId, setUser]=useState<string>('')
+  const firebaseConfig = {
+    apiKey: "AIzaSyCC4uY4mRBI5ySANdQShbnP28pooftoCHw",
+    authDomain: "movilessabado-f46c9.firebaseapp.com",
+    projectId: "movilessabado-f46c9",
+    storageBucket: "movilessabado-f46c9.appspot.com",
+    messagingSenderId: "152196470796",
+    appId: "1:152196470796:web:6d96e80a3f24d3eb08f629"
+
+  };
+  initializeApp(firebaseConfig);
+  function envioDatos(userId:string, score:string){
+    const db=getDatabase();
+    const reference=ref(db,'users/'+userId);
+    set(reference,{
+      highscore:score,
+    })
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>App de base de datos timepo real</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Ingresa el usuario'
+        value={userId}
+        onChangeText={setUser}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Ingresa el valor maximo'
+        value={score}
+        onChangeText={setScore}
+        keyboardType='numeric'
+      />
+      <Button
+        title='Envio de datos'
+        onPress={()=>envioDatos(userId,score)}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +55,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderColor:'gray',
+    borderWidth:3,
+    width:'80%'
+  }
 });
